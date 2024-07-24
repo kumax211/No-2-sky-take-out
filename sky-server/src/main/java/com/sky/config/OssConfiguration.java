0617fee2d2c09cd90785be0b8d58975cfb579f40
kumax211 <166813823+kumax211@users.noreply.github.com>
2024-07-24 20:40:36 +0900

    文件上传

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,31 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @projectName: sky-take-out
+ * @package: com.sky.config
+ * @className: OssConfigration
+ * @author: Eric
+ * @description: TODO
+ * @date: 2024/7/24 20:31
+ * @version: 1.0
+ */
+
+//配置类,用于创建AliossUtilUtil对象
+    @Configuration
+    @Slf4j
+public class OssConfiguration {
+        @Bean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象");
+       return new AliOssUtil(aliOssProperties.getEndpoint()
+                ,aliOssProperties.getAccessKeyId()
+        ,aliOssProperties.getAccessKeySecret()
+                ,aliOssProperties.getBucketName());
+    }
+}
